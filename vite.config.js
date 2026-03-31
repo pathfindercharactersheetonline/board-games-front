e5abb09d627be87399c81f289e9700390a83ba5c
Gennady <pathfindercharactersheetonline@gmail.com>
2026-03-31 15:00:40 +0300

    Fixing all configs and proxy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Все запросы, начинающиеся с /api, Vite будет перенаправлять на локальный питон
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
