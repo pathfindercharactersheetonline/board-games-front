279bf9bff82dd221aa3b39bcb57c8678fec2226a
Gennady <pathfindercharactersheetonline@gmail.com>
2026-03-31 13:25:41 +0300

    bugfix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,18 @@
+// const CONFIG = {
+//   // Пытаемся взять из окружения, если нет — ставим дефолт
+//   API_BASE_URL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+//   YANDEX_LOGIN_URL: `${import.meta.env.VITE_API_URL || 'http://localhost:8000'}/api/v1/auth/yandex/login`
+// };
+
+// export default CONFIG;
+
 const CONFIG = {
-  // Пытаемся взять из окружения, если нет — ставим дефолт
-  API_BASE_URL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
-  YANDEX_LOGIN_URL: `${import.meta.env.VITE_API_URL || 'http://localhost:8000'}/api/v1/auth/yandex/login`
+  // Оставляем пустым, чтобы fetch(`${CONFIG.API_BASE_URL}/api/v1/...`) 
+  // превратился в fetch(`/api/v1/...`)
+  API_BASE_URL: '', 
+  
+  // Здесь склеится в /api/v1/auth/yandex/login
+  YANDEX_LOGIN_URL: '/api/v1/auth/yandex/login'
 };
 
 export default CONFIG;
